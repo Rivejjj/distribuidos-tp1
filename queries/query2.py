034d5bfeb44e3eb7ad2503ae0507428e368bf804
Joaquin <jorivero@fi.uba.ar>
2024-04-21 18:23:59 -0300

    advances with regex query 2

--- queries/query2.py
+++ queries/query2.py
@@ -10,3 +10,23 @@ class query2:
 
     def get_decade(self,year):
         pass
+
+    def get_year_regex(self,text):
+        matches = []
+        if text == "":
+            return None
+        
+        i = 0
+        while i < len(text):
+            if text[i].isdigit():
+                digits = ""
+                while i < len(text) and text[i].isdigit() and len(digits) < 4:
+                    digits += text[i]
+                    i += 1
+                if len(digits) == 4:
+                    matches.append(int(digits))
+            else:
+                i += 1
+
+        return matches
+            
