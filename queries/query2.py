66aaae8ca4c9bfedac12eaf231ce7d1d32796c52
Joaquin <jorivero@fi.uba.ar>
2024-04-21 19:05:21 -0300

    advances in query2 logic

--- queries/query2.py
+++ queries/query2.py
@@ -1,21 +1,29 @@
+import math
+from ..messages import Book
+from ..messages import Review
 class query2:
-    def __init__(self, authors):
+    def __init__(self):
         self.authors = {} #authors,decades
 
-        
     def add(self, author, decade):
         if author not in self.authors:
-            self.authors[author] = []
-        self.authors[author].append(decade)
+            self.authors[author] = {}
+        self.authors[author][decade] = self.authors[author].get(decade,0) + 1
+
+    def add_book(self,book):
+        year = self.get_year_regex(book.publishedDate)
+        if year is None:
+            return
+        decade = self.get_decade(year[0])
+        for author in book.authors:
+            self.add(author,decade)
 
     def get_decade(self,year):
-        pass
+        return math.floor((year)%100/10)*10
 
     def get_year_regex(self,text):
-        matches = []
         if text == "":
             return None
-        
         i = 0
         while i < len(text):
             if text[i].isdigit():
@@ -24,9 +32,8 @@ class query2:
                     digits += text[i]
                     i += 1
                 if len(digits) == 4:
-                    matches.append(int(digits))
+                    return int(digits)
             else:
                 i += 1
-
-        return matches
-            
+        return None
+        
