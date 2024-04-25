83e3fb826e5c8211f18a197fd0da837aab0e6f2a
Joaquin <jorivero@fi.uba.ar>
2024-04-25 19:45:34 -0300

    data receiver basic functions

--- messages/book.py
+++ messages/book.py
@@ -2,15 +2,15 @@
 
 class Book:
     def __init__(self,title,description,authors,image,previewLink,publisher,publishedDate,infoLink,categories,ratingsCount):
-        title = ""
-        description = ""
-        authors = ""
-        image = ""
-        previewLink = "" 
-        publisher = ""
-        publishedDate = ""
-        infoLink = ""
-        categories = ""
-        ratingsCount = ""
+        self.title = title
+        self.description = description
+        self.authors = authors
+        self.image = image
+        self.previewLink = previewLink
+        self.publisher = publisher
+        self.publishedDate = publishedDate
+        self.infoLink = infoLink
+        self.categories = categories
+        self.ratingsCount = ratingsCount
 
         
--- messages/review.py
+++ messages/review.py
@@ -2,15 +2,13 @@
 
 class Review:
     def __init__(self, id, title, price, user_id, profileName, helpfulness, score, time, summary, text):
-        id = ""
-        title = ""
-        price = ""
-        user_id = ""
-        profileName = ""
-        helpfulness = ""
-        score = ""
-        time = ""
-        summary = ""
-        text = ""
-
-    
+        self.id = id
+        self.title = title
+        self.price = price
+        self.user_id = user_id
+        self.profileName = profileName
+        self.helpfulness = helpfulness
+        self.score = score
+        self.time = time
+        self.summary = summary
+        self.text = text
